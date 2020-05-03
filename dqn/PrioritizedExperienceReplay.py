2975d31cf2ff88187a43fb8bcd76fd8a53e78e0a
Jens Kutschera <jenskutschera@gmail.com>
2020-05-03 19:50:26 +0200

    add PrioritizedExperienceReplay.py using sumtree for improved performance and code clarity
    - sumtree is more efficient for large experience buffers esp. with random sampling
    - clarified PrioritizedExperienceReplay.py to improve structure and reusability
    - results of parameter search for PrioritizedExperienceReplay functionality achieving 15

--- dqn/PrioritizedExperienceReplay.py
+++ dqn/PrioritizedExperienceReplay.py
@@ -1,20 +1,26 @@
 import random
-import torch
+from collections import namedtuple
+
 import numpy as np
-
-from collections import namedtuple, deque
-
+import torch
+from SumTree import SumTree
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
+# based on https://github.com/rlcode/per
+
 class PrioritizedExperienceReplay:
     """Fixed-size buffer to store experience tuples."""
-    # Source: https://arxiv.org/abs/1511.05952
-    # pseudocode found on page 5 of the publication
+    """
+    e = 0.01
+    a = 0.6
+    beta = 0.4
+    beta_increment_per_sampling = 0.001
+    """
 
     def __init__(self, action_size, args):
-        """Initialize a ReplayBuffer object.
+        """Initialize a ExperienceReplay object.
 
         Params
         ======
@@ -23,40 +29,58 @@ class PrioritizedExperienceReplay:
             batch_size (int): size of each training batch
             seed (int): random seed
         """
-        self.action_size = action_size
-        self.memory = deque(maxlen=args.buffer_size)
-        self.batch_size = args.batch_size
-        # PRB: Add priority
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state",
-                                                                "priority", "done"])
         self.seed = random.seed(args.seed)
-        # PRB
-        self.priorities = deque(maxlen=args.buffer_size)
         self.max_priority = args.max_priority
         self.alpha = args.alpha
         self.beta = args.beta
+        self.alpha_increment = (args.alpha_end - args.alpha) / args.ann_length
+        self.beta_increment = (args.beta_end - args.beta) / args.ann_length
         self.eps = args.eps
+        self.i_episode = 0  # set from agent
+        self.max_priority = args.max_priority
+        self.tree = SumTree(args.buffer_size)
+        self.capacity = args.buffer_size
+        self.batch_size = args.batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+    def _get_priority(self, error):
+        # Sample transition probability P(i) = p_i^alpha / sum_k (p_k^alpha)
+        # instead, just safe priorities with the alpha squared already.
+        return (np.abs(error) + self.eps) ** self.alpha
 
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
+        experience = self.experience(state, action, reward, next_state, done)
+        p = self.max_priority
+        self.tree.add(p, experience)
 
-        # Store transition with maximal priority (line 6 pseudocode)
-        e = self.experience(state, action, reward, next_state, self.max_priority, done)
-        self.memory.append(e)
-        self.priorities.append(self.max_priority)
+        # done signals episode has ended. Then change alpha and beta
+        # alpha and beta outside 0...1 makes no sense
+        if done:
+            self.alpha = np.max(np.min([1., self.alpha + self.alpha_increment]), 0)
+            self.beta = np.max(np.min([1., self.beta + self.beta_increment]), 0)
 
     def sample(self):
         """Prioritized randomly sample a batch of experiences from memory."""
-        priorities = np.array(self.priorities)
-        # Sample transition probability P(i) = p_i^alpha / sum_k (p_k^alpha)
-        # pseudocode 9
-        assert(min(priorities) > 0)
-        probabilities = priorities ** self.alpha / sum(priorities ** self.alpha)
-        sample_indices = np.random.choice(np.arange(len(self.memory)), size=self.batch_size, p=probabilities)
         experiences = []
-        for i in sample_indices:
-            experiences.append(self.memory[i])
+        idxs = []
+        priorities = []
 
+        # sample a batch of random experiences by drawing a float between 0 and the sum of the tree
+        s = np.random.uniform(0, self.tree.total(), self.batch_size)
+        for i in range(self.batch_size):
+            (idx, p, experience) = self.tree.get(s[i])
+            priorities.append(p)
+            experiences.append(experience)
+            idxs.append(idx)
+
+        # pseudocode 10: compute importance-sampling weight w_j = (N + P(j))^-beta / max_i w_i
+        sampling_probabilities = priorities / self.tree.total()
+        is_weights = np.power(self.tree.n_entries * sampling_probabilities, -self.beta)
+        is_weights /= is_weights.max()
+
+        # put all the stuff on the device (gpu) using tensors.
+        # if pytorch is not used, just return numpy arrays and uncomment these lines
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
         rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
@@ -64,23 +88,14 @@ class PrioritizedExperienceReplay:
             device)
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(
             device)
-
-        # pseudocode 10: compute importance-sampling weight w_j = (N + P(j))^-beta / max_i w_i
-        is_weights = np.power(len(self.memory) * probabilities, -self.beta)
-        is_weights /= max(is_weights)
-
         is_weights = torch.from_numpy(is_weights).float().to(device)
+        return idxs, is_weights, states, actions, rewards, next_states, dones
 
-        return sample_indices, states, actions, rewards, next_states, is_weights, dones
-
-    def update_priorities(self, memory_indizes, priorities):
-        for i, p in zip(memory_indizes, priorities):
-            p += self.eps
-            state, action, reward, next_state, priority, done = self.memory[i]
-            self.memory[i] = self.experience(state, action, reward, next_state, p, done)
-            self.priorities[i] = p
-
+    def update_priorities(self, idxs, errors):
+        for idx, error in zip(idxs, errors):
+            p = self._get_priority(error)
+            self.tree.update(idx, p)
 
     def __len__(self):
         """Return the current size of internal memory."""
-        return len(self.memory)
+        return self.tree.n_entries
